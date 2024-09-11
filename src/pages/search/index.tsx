fe03cf1a957a38221a7c18fa155eb9bcde5c19f4
sryung <sryung1225@gmail.com>
2024-09-11 18:35:44 +0900

    [DAY2] 2. 라우팅 설정하기

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+  return (
+    <>
+      <h2>검색 결과 : {q}</h2>
+    </>
+  );
+}
