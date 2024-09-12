f99c027081620d18093a00452453abae1e81b680
sryung <sryung1225@gmail.com>
2024-09-12 17:42:35 +0900

    [DAY4] 2. 검색바 레이아웃 설정하기 (2) 검색바 구현

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,3 +1,33 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 export default function SearchBar() {
-  return <div>임시 서치바</div>;
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmitSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  return (
+    <form onSubmit={onSubmitSearch}>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요 ..."
+        onChange={onChangeSearch}
+      />
+      <button type="submit">검색</button>
+    </form>
+  );
 }
