34ca624665d26d6be59f1617452bae31bebb10eb
sryung <sryung1225@gmail.com>
2024-09-16 22:59:02 +0900

    [DAY6] 2. 서치 페이지 데이터 페칭 구현하기 (SSR)
    - getServerSideProps의 매개변수 context는 현재 브라우저로부터 받은 요청에 대한 모든 정보를 포함하고 있음
    - context를 활용해서 query string에 접근
    - fetchMovies()가 매개변수를 받으면 검색결과 데이터를 가져올 수 있도록 확장

--- src/lib/fetch-movies.ts
+++ src/lib/fetch-movies.ts
@@ -1,7 +1,12 @@
 import { MovieData } from "@/types";
 
-export default async function fetchMovies(): Promise<MovieData[]> {
-  const url = `http://localhost:12345/movie`;
+export default async function fetchMovies(q?: string): Promise<MovieData[]> {
+  let url = `http://localhost:12345/movie`;
+
+  if (q) {
+    url += `/search?q=${q}`;
+  }
+
   try {
     const response = await fetch(url);
     if (!response.ok) {
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,22 +1,34 @@
 import { ReactNode } from "react";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
-import movies from "@/dummy.json";
 import SearchableLayout from "@/components/searchable-layout";
 import MovieItem from "@/components/movie-item";
+import fetchMovies from "@/lib/fetch-movies";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
+  return {
+    props: {
+      movies,
+    },
+  };
+};
+
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { q } = router.query;
 
   return q ? (
-    <>
-      {/* <h2>검색 결과 : {q}</h2> */}
-      <ul className="grid grid-cols-3 gap-2 mt-5">
-        {movies.slice(0, 2).map((movie) => (
-          <MovieItem key={movie.id} {...movie} />
-        ))}
-      </ul>
-    </>
+    <ul className="grid grid-cols-3 gap-2 mt-5">
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </ul>
   ) : null;
 }
 
