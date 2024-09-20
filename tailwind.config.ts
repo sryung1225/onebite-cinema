d2160fcdb091dd2d558e00eef4f18743cc2e88f3
sryung <sryung1225@gmail.com>
2024-09-20 18:53:24 +0900

    [DAY9] 3. 글로벌 레이아웃 설정하기

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        "my-red": "#e50914",
       },
     },
   },
