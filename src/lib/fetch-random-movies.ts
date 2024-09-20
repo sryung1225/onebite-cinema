85d0ddcd69d89e8e5bb8a13c7af40ed5069a8327
sryung <sryung1225@gmail.com>
2024-09-20 17:43:05 +0900

    [DAY9] 0. 프로젝트 초기화

--- src/lib/fetch-random-movies.ts
+++
@@ -1,7 +0,0 @@
-import fetchData from "./fetch-data";
-import { MovieData } from "@/types";
-
-export default async function fetchRandomMovies(): Promise<MovieData[]> {
-  const endpoint = `movie/random`;
-  return fetchData<MovieData[]>(endpoint, []);
-}
